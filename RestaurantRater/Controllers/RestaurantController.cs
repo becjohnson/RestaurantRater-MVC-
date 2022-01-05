62d776ec09222f07b4631f0ff1e7f7289160e959
becjohnson <91417720+becjohnson@users.noreply.github.com>
2022-01-04 23:20:36 -0500

    Added Create Controller.. etc

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RestaurantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +10,28 @@ namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDbContext _db = new RestaurantDbContext();
         // GET: Restaurant/Index
         public ActionResult Index()
         {
+            return View(_db.Restaurants.ToList());
+        }
+        //Get Restaurant/Create
+        public ActionResult Create()
+        {
             return View();
         }
-
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
     }
 }
