c282f6a706d0bb352418cedb8614d93174841940
becjohnson <91417720+becjohnson@users.noreply.github.com>
2022-01-04 20:34:53 -0500

    Added Restauraunt Controller and Index View

--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -9,9 +10,13 @@ namespace RestaurantRater.Models
     public class Restaurant
     {
         [Key]
-        public int Id { get; set; }
+        public int RestaurantId { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public int Rating { get; set; }
+    }
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
