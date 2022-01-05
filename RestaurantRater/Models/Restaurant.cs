681ec97908f43cef7f3326887fe1bf34af36e4e7
becjohnson <91417720+becjohnson@users.noreply.github.com>
2022-01-04 20:29:30 -0500

    Initial Commit

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+}
